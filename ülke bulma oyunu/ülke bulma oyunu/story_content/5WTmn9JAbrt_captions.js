005681ded17da03e1f5b2a3f4e8b5829b52d56a4
ybertas <yavuz.b.ertas@gmail.com>
2026-05-06 10:38:36 +0300

    okey

---
+++ ülke bulma oyunu/ülke bulma oyunu/story_content/5WTmn9JAbrt_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5WTmn9JAbrt_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.108.36078.0%0D%0A%0D%0A00:00:00.100%20--%3E%2000:00:00.970%0D%0A[wrong]%0D%0A%0D%0A"}]})
